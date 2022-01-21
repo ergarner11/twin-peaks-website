a712d9ba73f8598b6b4d6b547a01abf5dfefae31
egarner <ergarner11@gmail.com>
2022-01-21 14:47:31 -0700

    Add pre-rendering

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HealthPlans from "./components/healthPlans";
@@ -21,4 +21,9 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
