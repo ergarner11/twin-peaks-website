37cbee4554deacf8e8007d11224ae951448609b9
egarner <ergarner11@gmail.com>
2023-10-30 12:10:28 -0600

    Update holiday hours

--- src/components/common/hours.jsx
+++ src/components/common/hours.jsx
@@ -10,9 +10,7 @@ function Hours({ darkmode, expanded }) {
     <div>
       <div className="d-flex">
         <ClockIcon
-          className={`me-3 ${
-            expanded ? "mt-2" : "align-self-center"
-          } ${className}`}
+          className={`me-3 ${expanded ? "mt-2" : "mt-3"} ${className}`}
         />
         <div>
           {expanded && (
@@ -27,6 +25,10 @@ function Hours({ darkmode, expanded }) {
           {!expanded && <p className={className}>Monday - Friday: 8am - 6pm</p>}
           <p className={className}>Saturday: 8am - 5pm</p>
           <p className={className}>Sunday: Closed</p>
+          <br />
+          <p className={className}>Thanksgiving (11/23): Closed</p>
+          <p className={className}>Christmas (12/25): Closed</p>
+          <p className={className}>New Year's Day (1/1): Closed</p>
         </div>
       </div>
     </div>
